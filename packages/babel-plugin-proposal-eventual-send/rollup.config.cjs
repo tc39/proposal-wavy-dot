86ee85074d41eec9491ee422ea27ff751ce9819e
Michael FIG <mfig@agoric.com>
2020-04-26 03:11:09 -0600

    fix: generate UMD bundles

--- packages/babel-plugin-proposal-eventual-send/rollup.config.cjs
+++ packages/babel-plugin-proposal-eventual-send/rollup.config.cjs
@@ -1,10 +1,21 @@
 const babelSource = require('../../scripts/rollup-plugin-babel-source');
+const resolve = require('rollup-plugin-node-resolve');
+const commonJs = require('rollup-plugin-commonjs');
+const rollupJson = require("@rollup/plugin-json");
 
-module.exports = {
+module.exports = [{
   input: 'src/index.js',
   output: {
     file: 'lib/index.js',
     format: 'cjs',
   },
-  plugins: [ babelSource() ]
-};
+  plugins: [ babelSource(), resolve(), commonJs(), rollupJson()]
+}, {
+  input: 'src/index.js',
+  output: {
+    file: `dist/index.umd.js`,
+    format: 'umd',
+    name: 'BabelPluginProposalEventualSend',
+  },
+  plugins: [babelSource(), resolve(), commonJs(), rollupJson()],
+}];
